e73d437ca09295b66aa6da019bb6c8033609dd4f
Luke <lukebrandon2004@gmail.com>
2024-11-20 14:39:27 +1000

    create_board() method added to board.py

--- board.py
+++ board.py
@@ -17,37 +17,59 @@ class Board():
                             if white_king:
                                 raise Exception("only one King per side")
                             white_king = True
+                            self._wking_position = square.get_position()
                         else:
                             if black_king:
                                 raise Exception("only one King per side")
                             black_king = True
+                            self._bking_position = square.get_position()
             if not white_king or not black_king:
                 raise Exception("Each side must have one King")
             self._board = board
         else: #make regular board
-            self._board = []
-            for i in range(0, 8):
-                row = []
-                if i == 1 or i == 6:
-                    color = WHITE if i == 1 else BLACK
-                    for j in range(0, 8):
-                        row.append(Pawn((j, i), color))
-                    self._board.append(row)
-                elif i == 0 or i == 7:
-                    color = WHITE if i == 0 else BLACK
-                    row.append(Rook((0, i), color))
-                    row.append(Knight((1, i), color))
-                    row.append(Bishop((2, i), color))
-                    row.append(Queen((3, i), color))
-                    row.append(King((4, i), color))
-                    row.append(Bishop((5, i), color))
-                    row.append(Knight((6, i), color))
-                    row.append(Rook((7, i), color))
-                    self._board.append(row)
-                else:
-                    for j in range(8):
-                        row.append(None)
-                    self._board.append(row)
+            self._board = self.construct_board()
+            self._wking_position = (5, 0)
+            self._bking_posiiton = (5, 7)
+
+        self._turn = WHITE
+
+    
+    def get_board(self) -> list[list[Piece]]:
+        return self._board
+
+    def get_turn(self) -> bool:
+        return self._turn
+
+    def change_turn(self) -> None:
+        self._turn = WHITE if self._turn == BLACK else BLACK
+
+
+    def construct_board(self) -> list[list[Piece]]:
+        board = []
+        for i in range(0, 8):
+            row = []
+            if i == 1 or i == 6:
+                color = WHITE if i == 1 else BLACK
+                for j in range(0, 8):
+                    row.append(Pawn((j, i), color))
+                board.append(row)
+            elif i == 0 or i == 7:
+                color = WHITE if i == 0 else BLACK
+                row.append(Rook((0, i), color))
+                row.append(Knight((1, i), color))
+                row.append(Bishop((2, i), color))
+                row.append(Queen((3, i), color))
+                row.append(King((4, i), color))
+                row.append(Bishop((5, i), color))
+                row.append(Knight((6, i), color))
+                row.append(Rook((7, i), color))
+                board.append(row)
+            else:
+                for j in range(8):
+                    row.append(None)
+                board.append(row)
+        return board
+
 
     def __str__(self) -> str:
         ans = ""
@@ -62,5 +84,9 @@ class Board():
             ans += "|\n"
         return ans
 
+    def in_check(self, player: bool) -> bool:
 
+        pass
+
+    
 
