27a5c24dc852b5b133e5fd6895e0c72f49f4c0f7
Luke <lukebrandon2004@gmail.com>
2025-01-20 14:44:52 +1000

    changed board color

--- constants.py
+++ constants.py
@@ -5,8 +5,8 @@ COLUMN_LETTERS = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
 COLUMNS = {"a" : 0, "b" : 1, "c" : 2, "d" : 3, "e" : 4, "f" : 5, "g" : 6, "h" : 7}
 
 
-GREEN = (0, 191, 70)
-CREAM = (252, 255, 131)
+GREEN = (119, 149, 86)
+CREAM = (235, 236, 208)
 COLORS = [GREEN, CREAM]
 GREY = (164, 164, 164)
 SQUARE_LENGTH = 64
